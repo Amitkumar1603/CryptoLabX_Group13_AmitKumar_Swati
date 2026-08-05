5fa76f54ca45f14e004aa22617e47078795ffeb2
Amit Kumar <amitkumar@Amits-MacBook-Air.local>
2026-08-05 19:14:56 +0530

    Initial project structure

---
+++ main.py
@@ -0,0 +1,26 @@
+def menu():
+    while True:
+        print("\n========== CryptoLabX ==========")
+        print("1. Encrypt")
+        print("2. Decrypt")
+        print("3. Attack")
+        print("4. Analyze")
+        print("5. Exit")
+
+        choice = input("Enter choice: ")
+
+        if choice == "1":
+            print("Coming Soon...")
+        elif choice == "2":
+            print("Coming Soon...")
+        elif choice == "3":
+            print("Coming Soon...")
+        elif choice == "4":
+            print("Coming Soon...")
+        elif choice == "5":
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid Choice")
+
+menu()
